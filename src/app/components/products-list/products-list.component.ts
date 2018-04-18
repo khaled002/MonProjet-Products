0a6c00a3849eedfc6bd3857ab65d41e347cb0cc1
Khaled Zouaoui <khaled.zouaoui11@gmail.com>
2018-04-18 16:44:26 +0200

    ajout du back office

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -14,7 +14,7 @@ export class ProductsListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.data = this._service.getAllProducts();
+    this._service.getAllProducts().subscribe(result => this.data = result);
 
   }
 }
